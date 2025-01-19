aaf1f5c66067d0a39f0f251f615231e67e1e454e
Jocelyn Wang <jtp.wang@gmail.com>
2025-01-20 04:43:55 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import Labs from "./Labs";
+import Lab1 from "./Labs/Lab1";
+import Lab2 from "./Labs/Lab2";
+import Lab3 from "./Labs/Lab3";
 import Kambaz from "./Kambaz";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+
 export default function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Kambaz" />} />
-          <Route path="/Labs/*" element={<Labs />} />
+          {/* Main Redirect */}
+          <Route path="/" element={<Navigate to="/Kambaz" />} />
+
+          {/* Kambaz Application */}
           <Route path="/Kambaz/*" element={<Kambaz />} />
+
+          {/* Redirect /Labs to /labs/lab1 */}
+          <Route path="/Labs" element={<Navigate to="/labs/lab1" />} />
+
+          {/* Individual Labs */}
+          <Route path="/labs/lab1" element={<Lab1 />} />
+          <Route path="/labs/lab2" element={<Lab2 />} />
+          <Route path="/labs/lab3" element={<Lab3 />} />
         </Routes>
       </div>
     </HashRouter>
-);}
+  );
+}
 
 
 
-
-// The App component is the root component that imports other components
-// to break up the HTML rendering task amongst a hierarchy of components
-
-// For how we are just rendering the Labs component,
-// which for now it's rendering just the Lab1 component
 
