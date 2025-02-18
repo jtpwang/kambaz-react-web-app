6b135e31eef46ccf8b72f80bb0187d0026cd36f0
Jocelyn Wang <jtp.wang@gmail.com>
2025-02-17 23:24:19 -0500

    complete 3.5.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -15,6 +15,22 @@ import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ForLoops from "./ForLoops";
 import MapFunction from "./MapFunction";
 import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Add from "./Add";
+import Square from "./Square"
+import Highlight from "./Highlight"
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
   return (
@@ -38,6 +54,31 @@ export default function Lab3() {
       <ForLoops />
       <MapFunction />
       <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+
+      <AddPathParameters />
+      <PathParameters />
+
+
     </div>
   );
 }
