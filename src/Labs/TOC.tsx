33eea52fb42c3f15a857f0d16563403bffe24fdc
Jocelyn Wang <jtp.wang@gmail.com>
2025-03-31 00:44:35 -0400

    complete 5.2.0

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -5,15 +5,24 @@ export default function TOC() {
   return (
     <ul className="nav nav-pills" id="wd-toc">
       <li className="nav-item"><a id="wd-a" href="#/Labs" className="nav-link">Labs</a></li>
+
       <li className="nav-item"><a id="wd-a1" href="#/Labs/Lab1"
         className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</a></li>
+
       <li className="nav-item"><a id="wd-a2" href="#/Labs/Lab2"
         className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</a></li>
+
       <li className="nav-item"><a id="wd-a3" href="#/Labs/Lab3"
         className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</a></li>
+
       <li className="nav-item"><a id="wd-a4" href="#/Labs/Lab4"
         className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>Lab 4</a></li>
+
+      <li className="nav-item"><a id="wd-a5" href="#/Labs/Lab5"
+        className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}>Lab 5</a></li>
+
       <li className="nav-item"><a id="wd-k" href="#/Kambaz" className="nav-link">Kambaz</a></li>
+
       <li className="nav-item"><a id="wd-github" href="https://github.com/jtpwang" target="_blank"
         className="nav-link">My GitHub</a></li>
     </ul>
