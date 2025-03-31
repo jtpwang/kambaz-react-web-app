457575c7bc9693e1b6a7d8f58710c04836858f6c
Jocelyn Wang <jtp.wang@gmail.com>
2025-03-31 15:09:33 -0400

    complete 5.2.3

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,17 +1,28 @@
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+
+import EnvironmentVariables from "./EnvironmentVariables";
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+
+
 export default function Lab5() {
     return (
-        <div id=
-            "wd-lab5">
+        <div id="wd-lab5">
             <h2>Lab 5</h2>
-            <div className=
-                "list-group">
-                <a href=
-                    "http://localhost:4000/lab5/welcome"
-                    className=
-                    "list-group-item">
+            <div className="list-group">
+                <a
+                    href={`${REMOTE_SERVER}/lab5/welcome`}
+                    className="list-group-item"
+                >
                     Welcome
                 </a>
-            </div><hr />
+            </div>
+            <hr />
+            <EnvironmentVariables />
+            <PathParameters />
+            <QueryParameters />
+            <WorkingWithObjects />
         </div>
     );
 }
