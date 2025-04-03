5afdf83634131086382d19e67624e81ecf0ca758
Jocelyn Wang <jtp.wang@gmail.com>
2025-04-03 12:17:16 -0400

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// [https://vite.dev/config/](https://vite.dev/config/)
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_REMOTE_SERVER': JSON.stringify('https://your-backend.onrender.com/')
+  }
 })
